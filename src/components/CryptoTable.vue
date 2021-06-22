67b38e2d8394487fd869b0e738d271e88507d5bd
anoob09 <aswarnkar.anup.swarnkar@gmail.com>
2021-06-22 12:02:24 +0530

    fetching coin list and rendering in the table with pagination

--- src/components/CryptoTable.vue
+++ src/components/CryptoTable.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="hello"></div>
+  <div class="overflow-auto">
+    <b-table
+      id="my-table"
+      :items="items"
+      :per-page="perPage"
+      :current-page="currentPage"
+      :fields="fields"
+      small
+    ></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "CryptoTable",
-  props: {
-    msg: String,
+  data() {
+    return {
+      items: [],
+      perPage: 10,
+      currentPage: 1,
+      fields: ["iconUrl", "name", "symbol", "price", "change"],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.coinranking.com/v1/public/coins/?limit=20")
+      .then((response) => {
+        this.items = response.data.data.coins;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
